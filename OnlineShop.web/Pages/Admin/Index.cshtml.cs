3276fb8fe4807bfc4ca513e56530852187c7ea59
DeV1 <danialshokouhmanesh@gmail.com>
2021-03-23 21:53:26 +0430

    admin panel layout added

---
+++ OnlineShop.web/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineShop.web.Pages.Admin
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+        
+    }
+}
